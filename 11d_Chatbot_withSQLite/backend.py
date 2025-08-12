3574370937eeee8a2912ac726428499aa459112f
Haider <Haiderali605@hotmail.com>
2025-08-12 23:29:02 +0900

    Implenting Sqlite for Permanent memory

---
+++ 11d_Chatbot_withSQLite/backend.py
@@ -0,0 +1,49 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from typing import TypedDict,Annotated,Literal
+from dotenv import load_dotenv, find_dotenv
+from pydantic import BaseModel, Field
+import operator
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+from langchain_core.messages import SystemMessage,HumanMessage,BaseMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+load_dotenv()
+import sqlite3
+
+_: bool = load_dotenv(find_dotenv())
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp")
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+###creating a database
+conn=sqlite3.connect(database="chatbot.db",check_same_thread=False)
+# Checkpointer
+#checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
+
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+##Give the  list of checkpoints
+    all_threads=set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+
+    return list(all_threads)
